b3dc57b34c0d9856d3e47f2f4692732b885bb294
DSGQR <amini.daniel@gmail.com>
2020-02-24 16:13:10 -0800

    Update Code - Page 82

--- Chapter02/Chapter/ft.js
+++ Chapter02/Chapter/ft.js
@@ -10,10 +10,13 @@
  *    Filename: ft.js
  */
 
- function resetForm() {
-   document.getElementById('photognum').value = 1;
-   document.getElementById('photoghrs').value = 2;
-   document.getElementById('membook').checked = false;
-   document.getElementById('reprodrights').checked = false;
-   document.getElementById('distance').value = 0;
- }
+function resetForm() {
+  document.getElementById('photognum').value = 1;
+  document.getElementById('photoghrs').value = 2;
+  document.getElementById('membook').checked = false;
+  document.getElementById('reprodrights').checked = false;
+  document.getElementById('distance').value = 0;
+}
+
+//  resets form when page is reloaded
+document.addEventListener('load', resetForm, false);
