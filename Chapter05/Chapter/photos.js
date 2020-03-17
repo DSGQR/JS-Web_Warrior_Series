243b45023881ba663a2c77c12d5020ae9a194a23
DSGQR <amini.daniel@gmail.com>
2020-03-17 04:42:23 -0700

    Add book example instrucitons

--- Chapter05/Chapter/photos.js
+++ Chapter05/Chapter/photos.js
@@ -15,6 +15,11 @@
 /* global variables */
 var photoOrder = [1,2,3,4,5];
 
+// ------------------------------- 4: DA ADDED ------------------------------- //
+// Page 318 - Step 9
+var figureCount = 3;
+
+
 // ------------------------------- 2: DA ADDED ------------------------------- //
 /* Add src values to img elements based on order specified in photoOrder array */
 function populateFigures() {
@@ -22,12 +27,24 @@ function populateFigures() {
    var filename;
    var currentFig;
 
-   // Define Variables & loops through to assign photos to each number
-   for (var i = 1; i < 4; i++) {
-      filename = 'images/IMG_0' + photoOrder[i] + 'sm.jpg';
-      currentFig = document.getElementsByTagName('img')[i - 1];
-      currentFig.src = filename;
+   // Page 318 - Step 10
+   if (figureCount === 3) {
+
+      // Define Variables & loops through to assign photos to each number
+      for (var i = 1; i < 4; i++) {
+         filename = 'images/IMG_0' + photoOrder[i] + 'sm.jpg';
+         currentFig = document.getElementsByTagName('img')[i - 1];
+         currentFig.src = filename;
+      } 
+   } else {
+      for (var i = 0; i < 5; i++) {
+         filename = 'images/IMG_0' + photoOrder[i] + 'sm.jpg';
+         currentFig = document.getElementsByTagName('img')[i];
+         currentFig.src = filename;
+      }
    }
+   // Page 319 - Step 12
+   figureCount = 5;
 }
 // ------------------------------------------------------------------------- //
 
@@ -53,6 +70,47 @@ function leftArrow() {
       }
       populateFigures();
    }
+}
+
+// ------------------------------- 3: DA ADDED ------------------------------- //
+// Switch Photo Feature From 4 -> 5 Photos
+function previewFive() {
+   var articleE1 = document.getElementsByTagName('article')[0];
+
+   // CREATE Figure & img elements for fifth image
+   var lastFigure = document.createElement('figure');
+   lastFigure.id = 'fig5';
+   lastFigure.style.zIndex = '5';
+   lastFigure.style.position = 'absolute';
+   lastFigure.style.right = '45px';
+   lastFigure.style.top = '67px';
+
+   var lastImage = document.createElement('img');
+   lastImage.width = '240';
+   lastImage.height= '135';
+
+   lastFigure.appendChild(lastImage);
+
+   articleE1.appendChild(lastFigure);
+
+   // Page 316 - Step 2
+   // Clone figure element for fifth image and edit to be first image.
+   var firstFigure = lastFigure.cloneNode(true);
+
+   // Page 317 - Step 3
+   firstFigure.id = 'fig1';
+   firstFigure.style.right = '';
+   firstFigure.style.left = '45px';
+
+   // Page 317 - Step 4
+   articleE1.appendChild(firstFigure);
+
+   // Page 318 - Step 8
+   // add appropriate src values to two new img elements
+   document.getElementsByTagName('img')[3].src = 
+   'images/IMG_0' + photoOrder[4] + 'sm.jpg';
+   document.getElementsByTagName('img')[4].src = 
+   'images/IMG_0' + photoOrder[0] + 'sm.jpg';
 }
 
 // ------------------------------- 1: DA ADDED ------------------------------- //
@@ -90,6 +148,14 @@ function createEventListeners() {
       mainFig.addEventListener('click', zoomFig, false);
    } else if (mainFig.attachEvent) {
       mainFig.attachEvent('onclick', zoomFig);
+   }
+
+   // Evet listener calling the previewFive() when user clicks 'show more'
+   var showAllButton = document.querySelector('#fiveButton p');
+   if (showAllButton.addEventListener) {
+      showAllButton.addEventListener('click', previewFive, false);
+   } else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent('onclick', previewFive);
    }
 }
 // --------------------------------------------------------------------------- //
